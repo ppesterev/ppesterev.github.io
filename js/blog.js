c4f7c15c46aadfdd4e933ec1cef496f63112a614
ppesterev <ppesterev@mail.ru>
2019-12-02 22:03:37 +0700

    big ol' refactor, logout sorta works but this is going nowhere fast

--- js/blog.js
+++ js/blog.js
@@ -8,124 +8,127 @@
     let loginUsernameInput = loginForm.querySelector('.fake-blog--name-input');
     let firstLogin = true; //HAAAX
 
+
+    let loggedInUserDislplay = mainScreen.querySelector('.fake-blog--logged-in-as');
+    let usernameDisplay = mainScreen.querySelector('.fake-blog--username');
+    
+    let postContainer = mainScreen.querySelector('.fake-blog--posts'); 
+    let postTemplate = mainScreen.querySelector('#fake-blog--post-template').content;
+    
+    let postForm = mainScreen.querySelector('.fake-blog--post-form');
+    let postTextarea = postForm.querySelector('.fake-blog--post-form textarea');
+
+    let visitForm = mainScreen.querySelector('.fake-blog--visit');
+    let goHome = mainScreen.querySelector('#fake-blog__home');
+
+    let loggedInUser;
+    let displayedUser;
+    let displayedPosts = [];
+
+
     loginForm.addEventListener('submit', function(evt) {
-
         evt.preventDefault();
-        username = loginUsernameInput.value;
-        if(!username)
+        if(!loginUsernameInput.value)
             return;
-
-        setupMain(username);
-        firstLogin = false;
+        
+        loggedInUser = loginUsernameInput.value;
+        if(firstLogin) {
+            setupMain();
+            firstLogin = false;
+        }
+        else {
+            showBlog(loggedInUser);
+        }
 
         loginScreen.classList.toggle('hidden');
         mainScreen.classList.toggle('hidden');
     });
 
-    function setupMain(loggedInUser) {
-        let loggedInUserDislplay = mainScreen.querySelector('.fake-blog--logged-in-as');
-        let usernameDisplay = mainScreen.querySelector('.fake-blog--username');
-        
-        let postContainer = mainScreen.querySelector('.fake-blog--posts'); 
-        let postTemplate = mainScreen.querySelector('#fake-blog--post-template').content;
-        
-        let postForm = mainScreen.querySelector('.fake-blog--post-form');
-        let postTextarea = postForm.querySelector('.fake-blog--post-form textarea');
+    function setupMain() {
 
-        let visitForm = mainScreen.querySelector('.fake-blog--visit');
-        let goHome = mainScreen.querySelector('#fake-blog__home');
-
-        let displayedUser = loggedInUser;
-        let displayedPosts = [];
-
-        loggedInUserDislplay.textContent = 'Logged in as ' + loggedInUser;
         showBlog(loggedInUser);
 
         postForm.addEventListener('submit', submitPostHandler);
         visitForm.addEventListener('submit', visitBlogHandler);
         goHome.addEventListener('click', goHomeHandler);
 
-        if(firstLogin) {    
             let logOut = mainScreen.querySelector('#fake-blog__logout');
             logOut.addEventListener('click', function(evt) {
                 evt.preventDefault();
-                postForm.removeEventListener('submit', submitPostHandler);
-                visitForm.removeEventListener('submit', visitBlogHandler);
-                goHome.removeEventListener('click', goHomeHandler);
-
                 loginScreen.classList.toggle('hidden');
                 mainScreen.classList.toggle('hidden');
             });
+    }
+
+    function goHomeHandler(evt) {
+        evt.preventDefault();
+        showBlog(loggedInUser);
+    }
+
+    function submitPostHandler(evt) {
+        evt.preventDefault();
+        let currentDateString = (new Date()).toLocaleString();
+        displayPost(loggedInUser, currentDateString, postTextarea.value);
+        displayedPosts.push({'username': loggedInUser,
+                             'date': currentDateString,
+                             'text': postTextarea.value
+                            });
+        document.activeElement.blur();
+        postTextarea.value = '';
+
+        localStorage.setItem('fake-blog__' + displayedUser, JSON.stringify(displayedPosts));
+    }
+
+    function visitBlogHandler(evt) {
+        evt.preventDefault();
+        let visitUsername = visitForm.querySelector('input[type="text"]').value;
+        // if(localStorage.getItem('fake-blog__' + visitUsername)) {
+            displayedUser = visitUsername;
+            showBlog(visitUsername);
+        // }
+        // else {
+        //     //TODO show error
+        // }
+
+    }
+
+    function showBlog(username) {
+        loggedInUserDislplay.textContent = 'Logged in as ' + loggedInUser;
+
+        displayedUser = username;
+        usernameDisplay.textContent = username;
+        let userDataString = localStorage.getItem('fake-blog__' + username);
+        displayedPosts = (userDataString ? JSON.parse(userDataString) : []);
+
+        // clear post container
+        while(postContainer.firstChild)
+            postContainer.removeChild(postContainer.firstChild);
+
+        // show existing posts
+        for(const post of displayedPosts) {
+            if(post.date && post.text)
+                displayPost(post.username, post.date, post.text);
         }
-        
-        function goHomeHandler(evt) {
-            evt.preventDefault();
-            showBlog(loggedInUser);
+    }
+
+    function displayPost(username, date, text) {
+
+        let newPost = postTemplate.querySelector('.fake-blog--post').cloneNode(true);
+        let postText = newPost.querySelector('.fake-blog--post__text');
+        let postUsername = newPost.querySelector('.fake-blog--post__username');
+        let postDate = newPost.querySelector('.fake-blog--post__date');
+
+        postUsername.textContent = username;
+        postDate.textContent = date;
+        postText.textContent = text;
+
+        if(loggedInUser === username) {
+            newPost.classList.add('my-post');
         }
-
-        function submitPostHandler(evt) {
-            evt.preventDefault();
-            let currentDateString = (new Date()).toLocaleString();
-            displayPost(loggedInUser, currentDateString, postTextarea.value);
-            displayedPosts.push({'username': loggedInUser,
-                                 'date': currentDateString,
-                                 'text': postTextarea.value
-                                });
-            document.activeElement.blur();
-            postTextarea.value = '';
-
-            localStorage.setItem('fake-blog__' + displayedUser, JSON.stringify(displayedPosts));
+        if(displayedUser === username) {
+            newPost.classList.add('owners-post');
         }
-
-        function visitBlogHandler(evt) {
-            evt.preventDefault();
-            let visitUsername = visitForm.querySelector('input[type="text"]').value;
-            // if(localStorage.getItem('fake-blog__' + visitUsername)) {
-                displayedUser = visitUsername;
-                showBlog(visitUsername);
-            // }
-            // else {
-            //     //TODO show error
-            // }
-
-        }
-
-        function showBlog(username) {
-            displayedUser = username;
-            usernameDisplay.textContent = username;
-            let userDataString = localStorage.getItem('fake-blog__' + username);
-            displayedPosts = (userDataString ? JSON.parse(userDataString) : []);
-
-            // clear post container
-            while(postContainer.firstChild)
-                postContainer.removeChild(postContainer.firstChild);
-
-            // show existing posts
-            for(const post of displayedPosts) {
-                if(post.date && post.text)
-                    displayPost(post.username, post.date, post.text);
-            }
-        }
-
-        function displayPost(username, date, text) {
-
-            let newPost = postTemplate.querySelector('.fake-blog--post').cloneNode(true);
-            let postText = newPost.querySelector('.fake-blog--post__text');
-            let postUsername = newPost.querySelector('.fake-blog--post__username');
-            let postDate = newPost.querySelector('.fake-blog--post__date');
-
-            postUsername.textContent = username;
-            postDate.textContent = date;
-            postText.textContent = text;
-
-            if(loggedInUser === username) {
-                newPost.classList.add('my-post');
-            }
-            if(displayedUser === username) {
-                newPost.classList.add('owners-post');
-            }
-            postContainer.insertBefore(newPost, postContainer.firstChild);
-        }
+        postContainer.insertBefore(newPost, postContainer.firstChild);
     }
 })();
 
