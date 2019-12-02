a4644a1332a9302265015c1e4ef4e67f6222a3f9
ppesterev <ppesterev@mail.ru>
2019-12-02 20:11:17 +0700

    Visiting & commenting on other blogs, styles for own and owner's posts

--- js/blog.js
+++ js/blog.js
@@ -12,43 +12,73 @@
         username = loginUsernameInput.value;
         if(!username)
             return;
-        let userDataString = localStorage.getItem('fake-blog__' + username);
-        let userPosts = (userDataString ? JSON.parse(userDataString) : []);
-        setupMain(username, userPosts);
+
+        setupMain(username);
         loginScreen.classList.toggle('hidden');
         mainScreen.classList.toggle('hidden');
     });
 
-    function setupMain(username, userPosts) {
-        if (!userPosts) {
-            userPosts = [];
-        }
+    function setupMain(loggedInUser) {
+        let loggedInUserDislplay = mainScreen.querySelector('.fake-blog--logged-in-as');
         let usernameDisplay = mainScreen.querySelector('.fake-blog--username');
-        usernameDisplay.textContent = username;
-
         let postContainer = mainScreen.querySelector('.fake-blog--posts'); 
         let postTemplate = mainScreen.querySelector('#fake-blog--post-template').content;
+        let displayedUser = loggedInUser;
+        let displayedPosts = [];
 
-        for(const post of userPosts) {
-            if(post.date && post.text)
-                newPost(username, post.date, post.text);
-        }
+        loggedInUserDislplay.textContent = 'Logged in as ' + loggedInUser;
+        showBlog(loggedInUser);
 
         let postForm = mainScreen.querySelector('.fake-blog--post-form');
         let postTextarea = postForm.querySelector('.fake-blog--post-form textarea');
         postForm.addEventListener('submit', function(evt) {
             evt.preventDefault();
             let currentDateString = (new Date()).toLocaleString();
-            newPost(username, currentDateString, postTextarea.value);
-            userPosts.push({'date': currentDateString, 'text': postTextarea.value});
+            displayPost(loggedInUser, currentDateString, postTextarea.value);
+            displayedPosts.push({'username': loggedInUser,
+                                 'date': currentDateString,
+                                 'text': postTextarea.value
+                                });
             document.activeElement.blur();
             postTextarea.value = '';
 
-            localStorage.setItem('fake-blog__' + username, JSON.stringify(userPosts));
+            localStorage.setItem('fake-blog__' + displayedUser, JSON.stringify(displayedPosts));
         });
 
-        function newPost(username, date, text) {
-            let newPost = postTemplate.cloneNode(true);
+        let visitForm = mainScreen.querySelector('.fake-blog--visit');
+        visitForm.addEventListener('submit', function(evt) {
+            evt.preventDefault();
+            let visitUsername = this.querySelector('input[type="text"]').value;
+            if(localStorage.getItem('fake-blog__' + visitUsername)) {
+                displayedUser = visitUsername;
+                showBlog(visitUsername);
+            }
+            else {
+                //TODO show error
+            }
+
+        })
+
+        function showBlog(username) {
+            displayedUser = username;
+            usernameDisplay.textContent = username;
+            let userDataString = localStorage.getItem('fake-blog__' + username);
+            displayedPosts = (userDataString ? JSON.parse(userDataString) : []);
+
+            // clear post container
+            while(postContainer.firstChild)
+                postContainer.removeChild(postContainer.firstChild);
+
+            // show existing posts
+            for(const post of displayedPosts) {
+                if(post.date && post.text)
+                    displayPost(post.username, post.date, post.text);
+            }
+        }
+
+        function displayPost(username, date, text) {
+
+            let newPost = postTemplate.querySelector('.fake-blog--post').cloneNode(true);
             let postText = newPost.querySelector('.fake-blog--post__text');
             let postUsername = newPost.querySelector('.fake-blog--post__username');
             let postDate = newPost.querySelector('.fake-blog--post__date');
@@ -57,6 +87,12 @@
             postDate.textContent = date;
             postText.textContent = text;
 
+            if(loggedInUser === username) {
+                newPost.classList.add('my-post');
+            }
+            if(displayedUser === username) {
+                newPost.classList.add('owners-post');
+            }
             postContainer.insertBefore(newPost, postContainer.firstChild);
         }
     }
@@ -78,17 +114,23 @@
 ]
 
 on login:
-    create userPosts
-    if username in localStorage:
-        populate userPosts with parsed JSON
-    setupMain(username, userPosts)
-        for each userpost
-            createPost(username, userpost.date, userpost.text)
+    
+    setupMain(loggedInUser)
+        displayedUser = loggedInUser
+        showBlog(loggedInUser)
+
         add listeners
         on submit new post:
-            createPost(username, Date(), textarea.value)
-            add new object to userPosts
-        on logout:
-            store userposts under fake-blog__<username>
+            addPost(username, Date(), textarea.value)
+            add new object to userposts
+            store stringified userposts
 
+        on switch user:
+           update displayedUser 
+           showBlog(displayedUser)
+
+        showBlog(username)
+            userposts = parse(localstorage.getitem(username))
+            for each userpost
+                addPost(username, userpost.date, userpost.text)
 */
