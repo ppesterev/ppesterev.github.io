2e4c7769f7ecdef0f553bed1fb2c98dce6c7ead0
ppesterev <ppesterev@mail.ru>
2021-06-12 02:32:30 +0700

    vue project setup

---
+++ src/components/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <header class="site-header">
+    <h1 class="page-title">Neat stuff</h1>
+    <p class="page-subtitle">A neat website for neat stuff</p>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style>
+body {
+  margin: 0;
+  font-size: 16px;
+  font-family: "Helvetica", sans-serif;
+  color: white;
+  background: #dddddd;
+}
+
+.site-header {
+  background: #444444;
+  padding: 20px;
+}
+
+.site-header,
+article {
+  border-top: 5px solid #f68b1e;
+}
+
+.page-title {
+  margin-top: 0;
+}
+
+.page-subtitle {
+  font-style: italic;
+}
+
+a {
+  color: orange;
+  text-decoration: none;
+}
+
+a:hover {
+  text-decoration: underline;
+}
+
+code,
+pre {
+  color: orange;
+  white-space: pre-wrap;
+}
+</style>
