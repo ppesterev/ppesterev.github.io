7802164dfe67cbf069157fd15938b250236a6948
ppesterev <ppesterev@mail.ru>
2021-06-12 18:19:52 +0700

    start breaking styles into bem modules

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,8 +3,8 @@ import React from "react";
 function App() {
   return (
     <header className="site-header">
-      <h1 className="page-title">Neat stuff</h1>
-      <p className="page-subtitle">A neat website for neat stuff</p>
+      <h1 className="site-header__title">Neat stuff</h1>
+      <p className="site-header__subtitle">A neat website for neat stuff</p>
     </header>
   );
 }
