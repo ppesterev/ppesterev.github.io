dabe1ceb19d03485e4a2f61f22611248a7bebdfa
ppesterev <ppesterev@mail.ru>
2021-06-12 18:52:02 +0700

    add site nav markup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,27 @@ function App() {
   return (
     <header className="site-header">
       <h1 className="site-header__title">Neat stuff</h1>
-      <p className="site-header__subtitle">A neat website for neat stuff</p>
+      <p className="site-header__subtitle">A frontend developer's portfolio</p>
+
+      <nav className="site-header__nav site-nav">
+        <ul className="site-nav__list">
+          <li className="site-nav__item">
+            <a href="" className="site-nav__link">
+              Projects
+            </a>
+          </li>
+          <li className="site-nav__item">
+            <a href="" className="site-nav__link">
+              Demos
+            </a>
+          </li>
+          <li className="site-nav__item">
+            <a href="" className="site-nav__link">
+              About
+            </a>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
