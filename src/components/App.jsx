b38d9d04f710c518c14f454f79299e79b68dd11f
ppesterev <ppesterev@mail.ru>
2021-06-13 00:30:23 +0700

    router scaffolding

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
 
 import Header from "./Header";
 
 function App() {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <Switch>
+        <Route path="/projects">
+          <h2>Projects</h2>
+        </Route>
+        <Route path="/demos">
+          <h2>Demos</h2>
+        </Route>
+        <Route path="/about">
+          <h2>About</h2>
+        </Route>
+        <Route to="*">
+          <Redirect to="/projects" />
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
