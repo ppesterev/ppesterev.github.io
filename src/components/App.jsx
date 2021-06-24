983f68c18b664b909a296be173b2c1b15f3f6a73
ppesterev <ppesterev@mail.ru>
2021-06-25 00:54:14 +0700

    add ProjectsView with a list of cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,25 +2,29 @@ import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
 import Header from "./Header";
+import ProjectsView from "./ProjectsView";
 
 function App() {
   return (
     <>
       <Header />
-      <Switch>
-        <Route path="/projects">
-          <h2>Projects</h2>
-        </Route>
-        <Route path="/demos">
-          <h2>Demos</h2>
-        </Route>
-        <Route path="/about">
-          <h2>About</h2>
-        </Route>
-        <Route to="*">
-          <Redirect to="/projects" />
-        </Route>
-      </Switch>
+      <main className="clamp-width">
+        <Switch>
+          <Route path="/projects">
+            <h2>Projects</h2>
+            <ProjectsView />
+          </Route>
+          <Route path="/demos">
+            <h2>Demos</h2>
+          </Route>
+          <Route path="/about">
+            <h2>About</h2>
+          </Route>
+          <Route to="*">
+            <Redirect to="/projects" />
+          </Route>
+        </Switch>
+      </main>
     </>
   );
 }
---
+++ src/components/ProjectsView.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function ProjectsView() {
+  return (
+    <div className="projects">
+      <ul className="projects__list">
+        {Array(4)
+          .fill(null)
+          .map((item, idx) => (
+            <li className="projects__item" key={idx}>
+              <article className="project">
+                <h3 className="project__title">Test project</h3>
+                <div className="project__content">
+                  <p className="project__description">
+                    Test project description
+                  </p>
+                  <img
+                    src="https://via.placeholder.com/200"
+                    alt=""
+                    width="200"
+                    height="200"
+                    className="project__image"
+                  />
+                </div>
+              </article>
+            </li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export default ProjectsView;
