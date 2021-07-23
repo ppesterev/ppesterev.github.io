e1afcd48f22bae6819dd64f1d3af4bdbe58a69d1
ppesterev <ppesterev@mail.ru>
2021-07-23 23:13:58 +0700

    rename mock data file

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
-import portfolioData from "../config.json";
+import portfolioData from "../portfolio-data";
 
 import Header from "./Header";
 import ProjectsView from "./ProjectsView";
