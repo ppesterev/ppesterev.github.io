c8652a40a558d6b93eeb284e615ae27369059c0b
ppesterev <ppesterev@mail.ru>
2021-08-03 23:48:23 +0700

    replace portfolio data with config file for fetching it from the repo

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 
-import portfolioData from "../portfolio-data";
+import { CONTENT_ROOT, CONTENT_DATA_FILE } from "../config";
 
 import Header from "./Header";
 import ProjectsView from "./ProjectsView";
 import DemosView from "./DemosView";
 
 function App() {
+  const [portfolioData, setPortfolioData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(`${CONTENT_ROOT}${CONTENT_DATA_FILE}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setIsLoading(false);
+        setPortfolioData(data);
+      });
+  }, []);
+
   return (
     <>
       <Header />
       <main className="clamp-width">
-        <Switch>
-          <Route path="/projects">
-            <ProjectsView
-              projects={portfolioData.projects}
-              tagData={portfolioData.tags}
-            />
-          </Route>
-          <Route path="/demos">
-            <DemosView demos={portfolioData.demos} />
-          </Route>
-          <Route path="/about">
-            <h2>About</h2>
-          </Route>
-          <Route to="*">
-            <Redirect to="/projects" />
-          </Route>
-        </Switch>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <Switch>
+            <Route path="/projects">
+              <ProjectsView
+                projects={portfolioData.projects}
+                tagData={portfolioData.tags}
+              />
+            </Route>
+            <Route path="/demos">
+              <DemosView demos={portfolioData.demos} />
+            </Route>
+            <Route path="/about">
+              <h2>About</h2>
+            </Route>
+            <Route to="*">
+              <Redirect to="/projects" />
+            </Route>
+          </Switch>
+        )}
       </main>
     </>
   );
---
+++ src/config.js
@@ -0,0 +1,5 @@
+const CONTENT_ROOT =
+  "https://raw.githubusercontent.com/ppesterev/ppesterev.github.io/content/";
+const CONTENT_DATA_FILE = "portfolio-data.json";
+
+export { CONTENT_ROOT, CONTENT_DATA_FILE };
