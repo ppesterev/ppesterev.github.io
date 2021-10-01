2dc3f7e853540264f9e3bba37bd689edb38dfe91
ppesterev <ppesterev@mail.ru>
2021-10-01 11:26:49 +0700

    an iteration on project card layout

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -4,31 +4,38 @@ function Project({ project, tagData }) {
   return (
     <article className="project">
       <h3 className="project__title">{project.title}</h3>
-      <div className="project__content">
-        <p className="project__description">{project.description}</p>
-        <div className="project__tags">
-          {project.tags?.map((tag) => (
-            <span
-              className="project__tag"
-              style={{
-                color: tagData[tag]?.color || "black",
-                backgroundColor: tagData[tag]?.bgColor
-                  ? `${tagData[tag]?.bgColor}80`
-                  : "#ffffff80"
-              }}
-            >
-              {tagData[tag]?.text || tag}
-            </span>
-          ))}
-        </div>
-        <img
-          src="https://via.placeholder.com/300x200"
-          alt=""
-          width="300"
-          height="200"
-          className="project__image"
-        />
+      <p className="project__description">{project.description}</p>
+      <div className="project__tags">
+        {project.tags?.map((tag) => (
+          <span
+            className="project__tag"
+            style={{
+              color: tagData[tag]?.color || "black",
+              backgroundColor: tagData[tag]?.bgColor
+                ? `${tagData[tag]?.bgColor}80`
+                : "#ffffff80"
+            }}
+          >
+            {tagData[tag]?.text || tag}
+          </span>
+        ))}
       </div>
+      <div className="project__links">
+        <a className="project__link" href="https://www.example.com">
+          Live demo
+        </a>
+        <br />
+        <a className="project__link" href="https://www.example.com">
+          Source code
+        </a>
+      </div>
+      <img
+        src="https://via.placeholder.com/600x800"
+        alt=""
+        width="600"
+        height="800"
+        className="project__image"
+      />
     </article>
   );
 }
