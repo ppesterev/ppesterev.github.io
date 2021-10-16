90c7fc69964df6261ddad210c81ff4fa0a427c61
ppesterev <ppesterev@mail.ru>
2021-10-17 02:51:30 +0700

    an iteration on gh log styles; add dates to events

--- src/components/GithubLogEvent.jsx
+++ src/components/GithubLogEvent.jsx
@@ -10,12 +10,26 @@ export default function GithubLogEvent({ event }) {
   const [ownerName, repoName] = event.repo.name.split("/");
   const isOwn = ownerName === "ppesterev";
 
+  // Thu Jan 01 1970 -> [Jan, 01]
+  const [month, date] = new Date(event.created_at)
+    .toDateString()
+    .split(" ")
+    .slice(1, 3);
+
   return (
     <div className="gh-event">
-      {`Created ${size} commit${size > 1 ? "s" : ""} in `}
-      <a href={`https://github.com/${event.repo.name}`}>
-        {isOwn ? repoName : event.repo.name}
-      </a>
+      <time
+        className="gh-event__date"
+        dateTime={new Date(event.created_at).toISOString()}
+      >
+        {date} {month}
+      </time>
+      <h4 className="gh-event__desc">
+        {`Created ${size} commit${size > 1 ? "s" : ""} in `}
+        <a href={`https://github.com/${event.repo.name}`}>
+          {isOwn ? repoName : event.repo.name}
+        </a>
+      </h4>
       <ul className="gh-event__commit-list">
         {event.payload.commits.map((commit) => (
           <li className="gh-event__commit-list-item">
