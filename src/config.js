3dfae1f63363c4c44672faea3afcb5dede1cb646
ppesterev <ppesterev@mail.ru>
2021-10-23 01:48:16 +0700

    use alternative content file in development

--- src/config.js
+++ src/config.js
@@ -1,5 +1,7 @@
 const CONTENT_ROOT =
-  "https://raw.githubusercontent.com/ppesterev/ppesterev.github.io/content/";
+  process.env.NODE_ENV === "development"
+    ? "https://raw.githubusercontent.com/ppesterev/ppesterev.github.io/content-dev/"
+    : "https://raw.githubusercontent.com/ppesterev/ppesterev.github.io/content/";
 const CONTENT_DATA_FILE = "portfolio-data.json";
 
 export { CONTENT_ROOT, CONTENT_DATA_FILE };
