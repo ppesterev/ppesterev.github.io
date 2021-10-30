391bfbd6b087277ddc71fac8c26a97122fe6f5cf
ppesterev <ppesterev@mail.ru>
2021-10-30 23:41:40 +0700

    extract about page header

---
+++ src/components/AboutHeader.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+import {
+  GitHub as GHIcon,
+  Mail as MailIcon,
+  Phone as PhoneIcon
+} from "react-feather";
+
+export default function AboutHeader({ className }) {
+  return (
+    <div className={`about-header ${className || ""}`}>
+      <img
+        src={new URL("../img/avatar-gs.png", import.meta.url)}
+        alt="My photo"
+        className="about-header__picture"
+      />
+      <p className="about-header__name">Pavel Pesterev</p>
+      <div className="about-header__links">
+        <a href="mailto:p.a.pesterev@gmail.com">
+          <MailIcon className="icon" /> p.a.pesterev@gmail.com
+        </a>
+        <a href="https://github.com/ppesterev" target="_blank">
+          <GHIcon className="icon" /> github.com/ppesterev
+        </a>
+        <a href="tel:+79132082850">
+          <PhoneIcon className="icon" /> +7 (913) 208-28-50
+        </a>
+      </div>
+    </div>
+  );
+}
--- src/components/AboutView.jsx
+++ src/components/AboutView.jsx
@@ -1,36 +1,13 @@
 import React from "react";
 
-import {
-  GitHub as GHIcon,
-  Mail as MailIcon,
-  Phone as PhoneIcon
-} from "react-feather";
-
+import AboutHeader from "./AboutHeader";
 import GithubLog from "./GithubLog";
 
 function AboutView({ ghEvents }) {
   return (
     <section className="about">
       <h2 className="visually-hidden">About</h2>
-      <div className="about__header about-header">
-        <img
-          src={new URL("../img/avatar-gs.png", import.meta.url)}
-          alt="My photo"
-          className="about-header__picture"
-        />
-        <p className="about-header__name">Pavel Pesterev</p>
-        <div className="about-header__links">
-          <a href="mailto:p.a.pesterev@gmail.com">
-            <MailIcon className="icon" /> p.a.pesterev@gmail.com
-          </a>
-          <a href="https://github.com/ppesterev" target="_blank">
-            <GHIcon className="icon" /> github.com/ppesterev
-          </a>
-          <a href="tel:+79132082850">
-            <PhoneIcon className="icon" /> +7 (913) 208-28-50
-          </a>
-        </div>
-      </div>
+      <AboutHeader className="about__header" />
       <GithubLog events={ghEvents} />
     </section>
   );
